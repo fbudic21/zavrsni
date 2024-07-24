ba8ec042ac5f6fe4fe312f878d97f91474213f08
fbudic21 <fbudic21@foi.hr>
2024-07-24 20:57:20 +0200

    Change compile and target SDK to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.fbudic21.monetizacija"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.fbudic21.monetizacija"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
