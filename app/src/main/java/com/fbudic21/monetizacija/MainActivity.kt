64224b59e6c5d8373fdfe19ea94deab414ab4f60
fbudic21 <fbudic21@foi.hr>
2024-07-24 21:34:13 +0200

    Add component to main screen for banner add

--- app/src/main/java/com/fbudic21/monetizacija/MainActivity.kt
+++ app/src/main/java/com/fbudic21/monetizacija/MainActivity.kt
@@ -3,27 +3,50 @@ package com.fbudic21.monetizacija
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.fbudic21.monetizacija.ui.theme.MonetizacijaTheme
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MonetizacijaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Spacer(modifier = Modifier.weight(1f))
+                        AdMobBanner(unitId = "ca-app-pub-3940256099942544/9214589741")
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun AdMobBanner(unitId: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier.fillMaxWidth(),
+        factory = { context ->
+            AdView(context).apply{
+                adUnitId = unitId
+                setAdSize(AdSize.BANNER)
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
 }
 
 @Composable
