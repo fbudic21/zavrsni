6582abfd47278a3e77e7f728abc876317ef436b8
fbudic21 <fbudic21@foi.hr>
2024-07-27 19:41:48 +0200

    Banner

--- app/src/main/java/com/fbudic21/monetizacija/MainActivity.kt
+++ app/src/main/java/com/fbudic21/monetizacija/MainActivity.kt
@@ -1,33 +1,51 @@
 package com.fbudic21.monetizacija
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.fbudic21.monetizacija.ui.theme.MonetizacijaTheme
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.VideoController
+import com.google.android.gms.ads.nativead.NativeAdOptions
+import com.google.android.gms.ads.nativead.NativeAdView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set test device IDs
+        val testDeviceIds = listOf("C07AFA7EA91B91BDB2B4920FACC59B5B")
+        val requestConfiguration = RequestConfiguration.Builder()
+            .setTestDeviceIds(testDeviceIds)
+            .build()
+
+        MobileAds.setRequestConfiguration(requestConfiguration)
+
+        MobileAds.initialize(this) { initializationStatus ->
+            Log.d("MyApp", "AdMob initialized: $initializationStatus")
+        }
+
         setContent {
             MonetizacijaTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Spacer(modifier = Modifier.weight(1f))
-                        AdMobBanner(unitId = "ca-app-pub-3940256099942544/9214589741")
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Column(modifier = Modifier.fillMaxSize()) {
+                            Spacer(modifier = Modifier.weight(1f))
+                            AdMobBanner(unitId = "ca-app-pub-3940256099942544/9214589741")
+                        }
                     }
                 }
             }
@@ -47,20 +65,4 @@ fun AdMobBanner(unitId: String, modifier: Modifier = Modifier) {
             }
         }
     )
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MonetizacijaTheme {
-        Greeting("Android")
-    }
 }
